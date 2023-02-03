5396bd43906ad037c4290e4bcb5840d7b7516db6
SonicL02 <you@example.com>
2023-02-03 04:34:01 +0300

    3.02.23

---
+++ 3.02.23.py
@@ -0,0 +1,52 @@
+
+# a = input()
+# c = a[0]
+# f = a[1]
+# counter = 0
+# for i in range(1, int(len(a))):
+#     f = a[i]
+#     if c is f:
+#         counter +=1
+#     c = f
+# print(counter)
+
+# s = input()
+# g = "ауоыиэяюёе"
+# c_g = 0
+# sg = "бвгджзйклмнпрстфхцчшщ"
+# c_sg = 0
+# s = s.lower()
+# for c in range(len(s)):
+#     if s[c] in g:
+#         c_g +=1
+#     elif s[c] in sg:
+#         c_sg +=1
+#     else:
+#         continue
+# print(f"Количество гласных букв равно {c_g}")
+# print(f"Количество согласных букв равно {c_sg}")
+
+# s = input()
+# str1 = s[:len(s)//2 + len(s)%2]
+# str2= s[len(s)//2 + len(s)%2::]
+# print(str2+str1)
+
+# s = input()
+# counter = 0
+# for i in range(len(s)):
+#     if s[i] in "abcdefghijklmnopqrstuvwxyz":
+#         counter +=1
+# print(counter)
+
+# s = input()
+# h1 = s.find("h")
+# h2 = s.rfind("h")
+# print(s[:h1] + s[h2 +1:])
+
+
+
+
+
+
+
+
